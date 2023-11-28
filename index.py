ff22c5dcee6752fade603d9e22be208d18484a96
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-27 21:53:14 -0500

    Update index.py

--- index.py
+++ index.py
@@ -57,4 +57,4 @@ def mostrar_tabla():
             </html>
                                             
                                             
-                                            ''')
+                                            ''', data=dict_data)
